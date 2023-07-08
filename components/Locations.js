839ab4fe868918d2bdebe5706f102888d73bdb0b
MekImEne <fi_mekideche@esi.dz>
2023-07-08 14:35:39 +0100

    Separate Locations component

---
+++ components/Locations.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { MapPinIcon } from 'react-native-heroicons/solid';
+
+export default function Locations({locations,showSearch}) {
+  return (
+    <>
+        {
+          locations.length>0 && showSearch  ? 
+            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+              {
+                locations.map((loc,index)=>{
+                  let showBorder = index+1 != locations.length;
+                  let borderClass = showBorder? ' border-b-2 border-b-gray-400':'';
+                  return (
+                    <TouchableOpacity
+                      key={index}
+                      className={"flex-row items-center border-0 p-3 px-4 mb-1" + borderClass}
+                      onPress={()=>handleLocation(loc)}
+                    >
+                      <MapPinIcon size="20" color="gray" />
+                      <Text className="text-black text-lg ml-2" >London, UK</Text>
+                    </TouchableOpacity>
+                  )
+                })
+              }
+            </View> 
+          : null
+        }
+    </>
+  )
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, Image, SafeAreaView, TouchableOpacity, Text } from "react-native";
+import { View, Image, SafeAreaView} from "react-native";
 import SearchSection from "../components/SearchSection";
-import { MapPinIcon } from "react-native-heroicons/solid";
+import Locations from "../components/Locations";
 
 export default function HomeScreen() {
   const [showSearch, setShowSearch] = useState(false);
@@ -25,28 +25,7 @@ export default function HomeScreen() {
 
       <SafeAreaView className="flex flex-1 mt-16">
         <SearchSection showSearch={showSearch} setShowSearch={setShowSearch} />
-        {
-          locations.length>0 && showSearch  ? 
-            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
-              {
-                locations.map((loc,index)=>{
-                  let showBorder = index+1 != locations.length;
-                  let borderClass = showBorder? ' border-b-2 border-b-gray-400':'';
-                  return (
-                    <TouchableOpacity
-                      key={index}
-                      className={"flex-row items-center border-0 p-3 px-4 mb-1" + borderClass}
-                      onPress={()=>handleLocation(loc)}
-                    >
-                      <MapPinIcon size="20" color="gray" />
-                      <Text className="text-black text-lg ml-2" >London, UK</Text>
-                    </TouchableOpacity>
-                  )
-                })
-              }
-            </View> 
-          : null
-        }
+        <Locations showSearch={showSearch} locations={locations} />
       </SafeAreaView>
     </View>
   );
